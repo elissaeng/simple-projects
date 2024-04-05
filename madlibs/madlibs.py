037c0ebc1564dc75ecae5e7c2626ab3f3a628955
elissaeng <elissaeng@gmail.com>
2024-04-05 13:32:22 -0700

    madlibs

---
+++ madlibs/madlibs.py
@@ -0,0 +1,11 @@
+# String concatenation
+
+name = input('Your name: ')
+main_course = input('Main course: ')
+dessert = input('Dessert: ')
+
+madlib = f"Hello {name}. Today's main course special is {main_course}, and for dessert we are offering {dessert}."
+
+print(madlib)
+
+
