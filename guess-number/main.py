322e5d86e1051551a86b128f2833e610547fbd01
elissaeng <elissaeng@gmail.com>
2024-04-09 12:23:47 -0700

    guess the number (user)

--- guess-number/main.py
+++ guess-number/main.py
@@ -34,6 +34,30 @@ import random
 # endregion
 
 
+
+# GUESS THE NUMBER (USER)
+# region ------------------------------------------------
+def computer_guess(x):
+    low = 1
+    high = x
+    feedback = ''
+    while feedback != 'c':
+        if low != high:
+            guess = random.randint(low, high)
+        else:
+            guess = low
+        feedback = input(f'Is {guess} too high (H), too low (L), or correct (C)? ').lower()
+        if feedback == 'h':
+            high = guess - 1
+        elif feedback == 'l':
+            low = guess + 1
+    print(f'Hooray! The computer guessed your number, {guess}, correctly!')
+
+computer_guess(10)
+
+#endregion
+
+
 # TEMPERATURE CHECKER
 # region ------------------------------------------------
 
